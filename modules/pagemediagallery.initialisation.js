e56c2a5afd177dee089ec0a5f66589b88c446e92
Pierre Boutet <boutetpierre@yahoo.fr>
2017-02-28 11:45:48 +0100

    correct init for new steps

--- modules/pagemediagallery.initialisation.js
+++ modules/pagemediagallery.initialisation.js
@@ -5,9 +5,18 @@ $(document).ready(function (){
 	var primaryGallery = new pagemediagallery.ui.PrimaryGallery();
 	primaryGallery.init();
 	
-	$(".msuploadContainer").each(function (i) {
+	$(".msuploadContainer").not('.multipleTemplateStarter .msuploadContainer').each(function (i) {
 		new pagemediagallery.ui.SecondaryGallery(this, primaryGallery);
 	});
+	
+	// add event on new step button, to appli VE on new steps
+	mw.hook( 'pf.addTemplateInstance' ).add( function(div) {
+		console.log('add step');
+			$(div).find('.msuploadContainer').each(function() {
+				console.log('add gallery on step');
+				new pagemediagallery.ui.SecondaryGallery(this, primaryGallery);
+			});
+		} );
 	
 	
 });
